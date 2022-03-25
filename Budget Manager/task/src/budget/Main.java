6750779545437732d7ceac86eb262d4fe6902e5c
Viet Anh <xkrules.pro@gmail.com>
2022-03-25 17:24:58 +0200

    Minimal changes.

--- Budget Manager/task/src/budget/Main.java
+++ Budget Manager/task/src/budget/Main.java
@@ -5,51 +5,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Scanner scanner1 = new Scanner(System.in);
-        String choice;
-        Manager budgetManager = new Manager();
-
-        //Main loop of program
-        while (true) {
-            budgetManager.printMenu();
-            choice = scanner.nextLine();
-
-            switch (choice) {
-                //Add income to balance
-                case "1":
-                    budgetManager.addIncome(scanner1);
-                    break;
-                //Add purchase to list and change balance
-                case "2":
-                    budgetManager.addPurchasesMenu(scanner, scanner1);
-                    break;
-                //Show list of purchases and total of it
-                case "3":
-                    budgetManager.showListOfPurchasesMenu(scanner);
-                    break;
-                //Show balance
-                case "4":
-                    budgetManager.showBalance();
-                    break;
-                //Save balance and list of purchases
-                case "5":
-                    budgetManager.showSaveMenu();
-                    break;
-                //Load from file purchases.txt balance and list of purchases
-                case "6":
-                    budgetManager.showLoadMenu();
-                    break;
-                //Sort purchases
-                case "7":
-                    budgetManager.showAnalyzeMenu(scanner);
-                    break;
-                //Exit
-                case "0":
-                    budgetManager.showExitMenu();
-                default:
-                    break;
-            }
-        }
+        new Manager().startManager();
     }
 }
